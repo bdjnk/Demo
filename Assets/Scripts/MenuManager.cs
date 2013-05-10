24e7d653ea9952a1ba5a906cb80a799b69347eba
Michael Plotke <michaelplotke@gmail.com>
2013-05-10 02:02:45 -0700

    GUI Skin + more menu fiddling

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,19 +5,32 @@ public class MenuManager : MonoBehaviour
 {
     public Vector2 scrollPosition = Vector2.zero;
 	
+	public GUISkin skin;
+	
 	public int serverCount = 40;
-	public int serverHeight = 30;
+	public int serverHeight = 25;
 	private int w, h;
 	private int scrollBarWidth = 16;
 	
 	void OnGUI()
 	{
+		GUI.skin = skin;
+		
 		w = Screen.width-scrollBarWidth;
 		
-		GUI.Label(new Rect(w*0.01f, 0, w-300, 40), "Name");
-		GUI.Label(new Rect(w-300+w*0.01f, 0, 100, 40), "Size");
-		GUI.Label(new Rect(w-200+w*0.01f, 0, 100, 40), "Players");
-		GUI.Label(new Rect(w-100+w*0.01f, 0, 100, 40), "Ping");
+		GUI.Button(new Rect(w*0.01f, 0, 100, 25), "Refresh");
+		GUI.Label(new Rect(w*0.01f+100, 0, 300, 25), "the server list to join a game, or");
+		GUI.Button(new Rect(w*0.01f+400, 0, 100, 25), "create one");
+		
+		GUI.BeginGroup(new Rect(w*0.01f, 25, w, 50));
+			
+		GUI.Label(new Rect(0, 0, w-300, 25), "Name");
+		GUI.Label(new Rect(w-300, 0, 100, 25), "Size");
+		GUI.Label(new Rect(w-200, 0, 100, 25), "Players");
+		GUI.Label(new Rect(w-100, 0, 100, 25), "Ping");
+		
+		GUI.EndGroup();
+		
 		scrollPosition = GUI.BeginScrollView(
 			new Rect(w*0.01f, 50, w*0.98f+scrollBarWidth, Mathf.Min(serverHeight*serverCount, Mathf.Round((Screen.height-50)/30)*30)),
 			scrollPosition, new Rect(0, 0, w*0.98f, serverHeight*serverCount), false, true);

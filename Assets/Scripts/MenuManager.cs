e0b3617e4e81fe0e7b89a654464a9c638bff2389
Michael Plotke <michaelplotke@gmail.com>
2013-05-10 02:55:54 -0700

    And another menu fiddling commit. It's looking good now.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -10,7 +10,7 @@ public class MenuManager : MonoBehaviour
 	public int serverCount = 40;
 	public int serverHeight = 25;
 	private int w, h;
-	private int scrollBarWidth = 16;
+	private int scrollBarWidth = 17;
 	
 	void OnGUI()
 	{
@@ -18,11 +18,17 @@ public class MenuManager : MonoBehaviour
 		
 		w = Screen.width-scrollBarWidth;
 		
-		GUI.Button(new Rect(w*0.01f, 0, 65, 25), "Refresh");
-		GUI.Label(new Rect(w*0.01f+70, 0, 200, 25), "the server list to join a game, or");
-		GUI.Button(new Rect(w*0.01f+260, 0, 80, 25), "create one");
+		GUI.BeginGroup(new Rect(w*0.01f, 5, w, 25));
 		
-		GUI.BeginGroup(new Rect(w*0.01f, 25, w, 50));
+		GUI.Button(new Rect(0, 0, 65, 25), "Refresh");
+		GUI.Label(new Rect(70, 0, 200, 25), "the server list to join a game, or");
+		GUI.Button(new Rect(260, 0, 80, 25), "create one.");
+		
+		GUI.EndGroup();
+		
+		GUI.Button(new Rect(Screen.width*0.99f-70, 5, 70, 25), "Settings");
+		
+		GUI.BeginGroup(new Rect(w*0.01f, 35, w, 25));
 			
 		GUI.Label(new Rect(0, 0, w-300, 25), "Name");
 		GUI.Label(new Rect(w-300, 0, 100, 25), "Size");
@@ -32,12 +38,13 @@ public class MenuManager : MonoBehaviour
 		GUI.EndGroup();
 		
 		scrollPosition = GUI.BeginScrollView(
-			new Rect(w*0.01f, 50, w*0.98f+scrollBarWidth, Mathf.Min(serverHeight*serverCount, Mathf.Round((Screen.height-50)/30)*30)),
+			new Rect(w*0.01f, 60, Screen.width*0.98f, Mathf.Min(serverHeight*serverCount, Mathf.Round((Screen.height-60)/30)*30)),
 			scrollPosition, new Rect(0, 0, w*0.98f, serverHeight*serverCount), false, true);
 		
 		for (int i = 0; i < serverCount; i++)
 		{
-			GUI.BeginGroup(new Rect(0, serverHeight*i, w*0.98f, serverHeight*(i+1)), new GUIContent());
+			//GUI.BeginGroup(new Rect(0, serverHeight*i, w*0.98f, serverHeight*(i+1)), new GUIContent());
+			GUI.BeginGroup(new Rect(0, serverHeight*i, w*0.98f, serverHeight), new GUIContent());
 			
 			GUI.Label(new Rect(    0, 0, w-300, serverHeight), "Test Server Name #"+i);
 			GUI.Label(new Rect(w-300, 0, 100, serverHeight), "Size");
